e8ca22437dd42559905bc1b9156243fe0af5b3f3
allison <allison.villa@outlook.com>
2022-02-05 23:04:35 -0500

    made initial commit on js branch, feature planning

---
+++ script.js
@@ -0,0 +1,8 @@
+// TO-DO
+
+// Planned Features:
+// Email newsletter subscribe form after scrolling through homepage
+// Thank user for posting comment on form submit on blog page
+
+// If I have time:
+// Create new comment post 
