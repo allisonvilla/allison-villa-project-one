70b9dbb3a689f1decaa99f4baf06bd565f89fb00
allison <allison.villa@outlook.com>
2022-02-07 21:44:14 -0500

    added newsletter pop up

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-// TO-DO
+// Currently, unless you comment out Feature One, Feature Two will not work because of a TypeError (variables defined in Feature One are null on the blog page)
 
-// Planned Features:
-// Email newsletter subscribe form after scrolling through homepage
+// FEATURE ONE: Newsletter Sign-Up on Homepage Scroll 
 
-// COMMENT SUBMIT CONFIRMATION 
 // Store HTML elements in variables
-const commentSubmit = document.querySelector('#commentSubmit');
+const newsletterPopUp = document.querySelector('#newsletterPopUp');
+const closeSpan = document.querySelector('.close'); 
+
+// Variable that checks if the pop up has already happened 
+let hasPoppedUp = false; 
+
+// After scrolling 900px from the top, show newsletter pop up
+window.onscroll = function() {
+    if (window.pageYOffset > 900 && hasPoppedUp == false) {
+        newsletterPopUp.style.visibility = 'visible'; 
+        hasPoppedUp = true; 
+    }
+}
+
+// Hide pop up when user clicks "I'm not interested"
+closeSpan.addEventListener ('click', function() {
+    newsletterPopUp.style.visibility = 'hidden'; 
+}); 
+
+// FEATURE TWO: Comment Confirmation Message on Blog Page 
+
+// Store HTML elements in variables
 const tyMessage = document.querySelector('#thankYouMessage');
 const commentForm = document.querySelector('#commentForm'); 
 const commenterName = document.querySelector('#name'); 
@@ -18,7 +37,7 @@ commentForm.addEventListener ('submit', function(e) {
     let username = commenterName.value; 
     // Show the thank you message
     tyMessage.style.display = 'inline-block'; 
-    tyMessage.textContent = `Thanks for submitting a comment, ${username}! It has been sent for approval. 💌`; 
+    tyMessage.textContent = `Thanks for submitting a comment, ${username}! It has been sent for approval. 😊`; 
     // Clear the form
     commenterName.value = '';
     commenterEmail.value = '';
